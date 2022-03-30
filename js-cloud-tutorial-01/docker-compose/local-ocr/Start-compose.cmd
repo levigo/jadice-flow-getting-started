563c7e3d14b6163056c70827c52279f19beec62f
Simon Welsch <s.welsch@levigo.de>
2022-03-30 16:03:18 +0200

    feat(t-01): use .env for both linux and windows

--- js-cloud-tutorial-01/docker-compose/local-ocr/Start-compose.cmd
+++ js-cloud-tutorial-01/docker-compose/local-ocr/Start-compose.cmd
@@ -1,17 +1,7 @@
 @echo off
-set JS_DC_PATH=.
-set JS_DOCKER_REGISTRY_PROXY=""
-set JS_DOCKER_REGISTRY_JADICE=registry.jadice.com/
-set JS_DIRECTOR_ACCESS_TOKEN=THE-JADICE-SERVER-ACCESS-TOKEN
-set MINIO_ENDPOINT=YOUR-IP
-set MINIO_PORT=9000
-set MINIO_ACCESS_KEY=UXBQvo9GfxWqzeInxLalvRWpEXAMPLEKEY
-set MINIO_SECRET_KEY=ki0BOUQn7XTfJphbXvG59kEbLsy89V5DkOoH7A6bEvwI6qzfEXAMPLEKEY
-set MINIO_PROTOCOL=http
-
 echo Starting docker compose for jadice server with OCR
-
-docker login %JS_DOCKER_REGISTRY_JADICE%
+echo Login to levigo container registry
+docker login registry.jadice.com
 
 IF NOT EXIST mariadb-data mkdir mariadb-data
 IF NOT EXIST minio-data mkdir minio-data
--- js-cloud-tutorial-01/docker-compose/local-ocr/start-compose.sh
+++ js-cloud-tutorial-01/docker-compose/local-ocr/start-compose.sh
@@ -2,17 +2,22 @@
 
 set -eu pipefail ;
 
+_login_docker_registry(){
+  echo ">>>[start-compose] login to levigo container registry" ;
+  docker login registry.jadice.com ;
+  return 0 ;
+} ;
+
 _configure_container_mounts(){
-  echo ;
+  echo ">>>[start-compose] configure container mounts" ;
   mkdir -p ./mariadb-data/ ;
   mkdir -p ./minio-data/ ;
-
   return 0 ;
 } ;
 
 _start_docker_compose_stack(){
-  echo ">>>[dc-deploy] start docker-compose stack and follow logs" ;
-  echo ">>>[dc-deploy] (You can disconnect from logs with Ctrl+C without stopping containers)" ;
+  echo ">>>[start-compose] start docker-compose stack and follow logs" ;
+  echo ">>>[start-compose] (You can disconnect from logs with Ctrl+C without stopping containers)" ;
   docker-compose up -d ;
   docker-compose logs -f
   return 0 ;
@@ -20,6 +25,7 @@ _start_docker_compose_stack(){
 
 
 _main() {
+  _login_docker_registry ;
   _configure_container_mounts ;
   _start_docker_compose_stack ;
   return 0 ;
